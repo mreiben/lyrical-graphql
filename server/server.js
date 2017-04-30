3e61127b8cf8b5d75dd451e942307219dd64c791
mreiben <jason.eiben@gmail.com>
2017-04-30 16:26:08 -0400

    wire react component to graphql

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://user2:user2@ds123381.mlab.com:23381/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }

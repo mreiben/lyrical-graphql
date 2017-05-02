64eec7b4020d35d760ce127f1b27257b14440231
mreiben <jason.eiben@gmail.com>
2017-05-01 21:16:12 -0400

    attach mutations to songlist

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,11 +6,17 @@ import fetchSongsQuery from '../queries/fetchSongs'
 
 class SongList extends Component {
 
+  onSongDelete(id){
+    this.props.mutate(
+      { variables: { id } }).then(()=> {this.props.data.refetch()});
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({id, title}) => {
       return(
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={()=> this.onSongDelete(id)}>delete</i>
         </li>
       );
     });
@@ -37,4 +43,15 @@ class SongList extends Component {
   }
 }
 
-export default graphql(fetchSongsQuery)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id:$id){
+      id
+    }
+  }
+`;
+
+
+export default graphql(mutation)(
+  graphql(fetchSongsQuery)(SongList)
+);
